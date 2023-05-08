9f7e3b46d17fa6426e8d1273c4ae0ee27ccf39b4
Ramy Hcini <hssini.ramy@gmail.com>
2023-05-08 20:34:10 +0200

    update docker

--- build_docker.sh
+++ build_docker.sh
@@ -2,7 +2,7 @@
 
 set -e
  
-docker build -t pequin -f docker/Dockerfile .
+docker build -t pequin_jessie -f docker/Dockerfile .
 
 # Test created image
-docker run -it pequin bash -c 'cd $PEQUIN/pepper && ./pepper_compile_and_setup_V.sh mm_pure_arith mm_pure_arith.vkey mm_pure_arith.pkey && ./pepper_compile_and_setup_P.sh mm_pure_arith'
+docker run -it pequin_jessie bash -c 'cd $PEQUIN/pepper && ./pepper_compile_and_setup_V.sh mm_pure_arith mm_pure_arith.vkey mm_pure_arith.pkey && ./pepper_compile_and_setup_P.sh mm_pure_arith'
